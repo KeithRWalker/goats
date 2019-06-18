7dfb12b854114592b26fe1ecf62ca360769caede
Keith Walker <Keithrwalker418@gmail.com>
2019-06-17 21:41:33 -0500

    props added

--- src/components/GoatCorral/GoatCorral.js
+++ src/components/GoatCorral/GoatCorral.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import goatShape from '../../helpers/propz/goatShape';
 
 import './GoatCorral.scss';
 
 import Goat from '../Goat/Goat';
 
 class GoatCorrall extends React.Component {
+  static propTypes = {
+    goats: PropTypes.arrayOf(goatShape.goatShape),
+  };
+
   render() {
     // const makeGoats = this.props.makeGoats;
     const { goats } = this.props;
